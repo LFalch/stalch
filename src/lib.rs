37ce1e8f5d62ef3634686218bb45c5fbca583eac
LFalch <lucas@wasd.dk>
2017-08-20 03:30:05 +0200

    Add more commands and value types

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,16 @@ use std::fs::File;
 use std::io::Read;
 
 struct State {
-    stack: Vec<Value>
+    stack: Vec<Value>,
+    wip_block: Option<Vec<String>>
 }
 
 impl State {
     fn new() -> Self{
-        State{stack: Vec::new()}
+        State {
+            stack: Vec::new(),
+            wip_block: None
+        }
     }
 }
 
@@ -42,26 +46,69 @@ mod value;
 use value::Value;
 use value::Value::*;
 
+fn binop<F: FnOnce(Value, Value) -> Value>(s: &mut State, f: F) {
+    let a = s.stack.pop().unwrap();
+    let b = s.stack.pop().unwrap();
+
+    s.stack.push(f(a, b))
+}
+
+use std::ops::*;
+
 fn run_command(state: &mut State, cmd: &str) {
-    if cmd.starts_with('"') {
+    if let ref mut b @ Some(_) = state.wip_block {
+        if cmd == "}" || cmd == "]" || cmd == "end" {
+            state.stack.push(Block(b.take().unwrap()));
+        } else {
+            b.as_mut().unwrap().push(cmd.to_owned());
+        }
+    } else if cmd.starts_with('"') {
         state.stack.push(Str(cmd[1..cmd.len()-1].to_owned()));
     } else if let Ok(n) = cmd.parse::<f64>() {
         state.stack.push(Num(n));
-    } else if let "*0" = cmd {
-        state.stack.push(Value::Null);
     } else {
-        match cmd {
-            "dup" => {
+        match &*cmd.to_lowercase() {
+            "{" | "[" | "do" => state.wip_block = Some(Vec::new()),
+            "T" | "true" => state.stack.push(Num(1.)),
+            "t" | "f" | "false" => state.stack.push(Num(0.)),
+            "¤" | "null" | "nil" => state.stack.push(Value::Null),
+            ";" | "dup" => {
                 let dup = state.stack.last().cloned().unwrap();
                 state.stack.push(dup);
-            },
-            "prnt" => {
-                match state.stack.pop().unwrap() {
-                    Str(s) => println!("{}", s),
-                    Num(n) => println!("{}", n),
-                    Null => println!("NULL")
+            }
+            "!" | "not" => {
+                let a = state.stack.pop().unwrap();
+                state.stack.push(!a);
+            }
+            "if" | "?" => {
+                let condition = state.stack.pop().unwrap();
+                let when_false = state.stack.pop().unwrap();
+                let when_true = state.stack.pop().unwrap();
+
+                state.stack.push(if condition.as_bool() {
+                    when_true
+                } else {
+                    when_false
+                });
+            }
+            "()" => {
+                match state.stack.pop() {
+                    Some(Block(b)) => {
+                        for cmd in b {
+                            run_command(state, &cmd);
+                        }
+                    }
+                    _ => panic!("No block on stack")
                 }
             }
+            "<" => state.stack.push(Str("user input".to_owned())),
+            ">" | "wrte" => print!("{}", state.stack.pop().unwrap()),
+            "_" | "prnt" => println!("{}", state.stack.pop().unwrap()),
+            "+" | "add" => binop(state, Add::add),
+            "-" | "sub" => binop(state, Sub::sub),
+            "*" | "mul" => binop(state, Mul::mul),
+            "/" | "div" => binop(state, Div::div),
+            "%" | "rem" => binop(state, Rem::rem),
             _ => panic!("Unknown cmd `{}'", cmd)
         }
     }
--- src/value.rs
+++ src/value.rs
@@ -1,6 +1,115 @@
+use std::ops::*;
+use std::fmt;
+
 #[derive(Debug, Clone)]
 pub enum Value {
     Num(f64),
     Str(String),
+    Block(Vec<String>),
     Null
+}
+
+impl Value {
+    pub fn as_bool(&self) -> bool {
+        match *self {
+            Num(n) => !n.is_nan() || n != 0.,
+            Str(ref s) => !s.is_empty(),
+            Block(_) => true,
+            Null => false
+        }
+    }
+    #[inline(always)]
+    pub fn from_bool(b: bool) -> Self {
+        match b {
+            true => Num(1.),
+            false => Num(0.),
+        }
+    }
+}
+
+use Value::*;
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Num(ref n) => n.fmt(f),
+            Str(ref s) => s.fmt(f),
+            Block(_) => write!(f, "[code block]"),
+            Null       => "NULL".fmt(f)
+        }
+    }
+}
+
+impl Not for Value {
+    type Output = Self;
+    fn not(self) -> Self {
+        Value::from_bool(self.as_bool())
+    }
+}
+
+impl Add for Value {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        match (self, other) {
+            (Null, b) => b,
+            (a, Null) => a,
+            (Str(a), b)  => Str(format!("{}{}", a, b)),
+            (Num(a), Str(b))  => Str(format!("{}{}", a, b)),
+            (Num(a), Num(b)) => Num(a + b),
+            (Block(mut a), Block(b)) => {
+                a.extend(b);
+                Block(a)
+            }
+            (Block(_), _) | (_, Block(_)) => panic!("Can't add blocks")
+        }
+    }
+}
+
+impl Mul for Value {
+    type Output = Self;
+    fn mul(self, other: Self) -> Self {
+        match (self, other) {
+            (Str(s), Num(n)) | (Num(n), Str(s))  => Str(s.repeat(n as usize)),
+            (Num(a), Num(b)) => Num(a * b),
+            (Num(n), Block(b)) | (Block(b), Num(n)) => {
+                let mut ret = Vec::with_capacity(b.len() * n as usize);
+                let extra = b.clone();
+                for _ in 0..n as usize {
+                    ret.extend(extra.clone());
+                }
+                Block(ret)
+            },
+            _ => Null,
+        }
+    }
+}
+
+impl Sub for Value {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        match (self, other) {
+            (Num(a), Num(b)) => Num(a * b),
+            _ => Null,
+        }
+    }
+}
+
+impl Div for Value {
+    type Output = Self;
+    fn div(self, other: Self) -> Self {
+        match (self, other) {
+            (Num(a), Num(b)) => Num(a * b),
+            _ => Null,
+        }
+    }
+}
+
+impl Rem for Value {
+    type Output = Self;
+    fn rem(self, other: Self) -> Self {
+        match (self, other) {
+            (Num(a), Num(b)) => Num(a * b),
+            _ => Null,
+        }
+    }
 }
